34b50507d0a8b941f135faff24c1e4b91a1cf9a6
Ivan <vanchysz1@gmail.com>
2026-09-21 03:33:34 -0600

    Actualizar el nombre de la versión del caché a 'tarifas-cache-v2.1'

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Nombre de la versión del caché
-const CACHE_NAME = 'tarifas-cache-v2';
+const CACHE_NAME = 'tarifas-cache-v2.1';
 
 // Archivos para precargar y servir offline
 const ASSETS_TO_CACHE = [
